0bc89ebecdde504d140dc9021878a55d0a211d4a
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2023-12-25 17:30:33 +0100

    move to pydantic

--- metadata/vendors.py
+++ metadata/vendors.py
@@ -1,11 +1,35 @@
 """Cloud compute resource vendors."""
 
+from .location import Location
 
-class Vendor(object):
-    """Base class for cloud compute resource vendors."""
+from typing import Optional
+from pydantic import BaseModel, HttpUrl
 
-    name: str = NotImplemented
-    logo: str = NotImplemented
-    homepage: str = NotImplemented
-    location = str None
-    found_date = NotImplemented
+
+class Vendor(BaseModel):
+    """Base class for cloud compute resource vendors.
+
+    Examples:
+        >>> aws_loc = Location(country='US', city='Seattle', address_line1='410 Terry Ave N')
+        >>> aws = Vendor(identifier='aws', name='Amazon Web Services', homepage='https://aws.amazon.com', location=aws_loc, found_date=2002)
+    """  # noqa: E501
+
+    identifier: str
+    name: str
+    logo: Optional[HttpUrl] = None  # TODO upload to cdn.sparecores.com
+    homepage: HttpUrl
+    location: Location
+    found_date: int
+
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        try:
+            self.methods = __import__('providers.' + self.identifier)
+        except:
+            raise NotImplementedError('Unsupported vendor')
+            pass
+
+    def get_instance_types(self):
+        return self.methods.get_instance_types()
+
