16ed47b738fd551d6f11b42fa56f844a46b0689d
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2024-01-02 16:24:49 +0100

    enable caching

--- sc_crawler/app.py
+++ sc_crawler/app.py
@@ -3,6 +3,11 @@ from sqlmodel import Session
 from .database import create_db_and_tables, engine
 from .vendors import aws
 
+# enable caching
+from cachier import set_default_params
+
+set_default_params(caching_enabled=True)
+
 
 def crawl():
     create_db_and_tables()
@@ -10,6 +15,7 @@ def crawl():
         for vendor in [aws]:
             vendor.get_datacenters()
             vendor.get_zones()
+            vendor.get_instance_types()
             session.add(vendor)
             session.commit()
 
