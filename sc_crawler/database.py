97d96ea9c6a0b9075d2069b5f8409d63e6e61b3d
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2024-01-02 16:24:56 +0100

    enable custom serializers

--- sc_crawler/database.py
+++ sc_crawler/database.py
@@ -1,9 +1,15 @@
+from json import dumps
 from os import getenv
 from sqlmodel import SQLModel, create_engine
 
 
+def custom_serializer(x):
+    """Use JSON serializer defined in custom objects."""
+    return dumps(x, default=lambda x: x.__json__())
+
+
 DB_ENGINE = getenv("SC_CRAWLER_DB_ENGINE", "sqlite:////tmp/sc_crawler.db")
-engine = create_engine(DB_ENGINE)
+engine = create_engine(DB_ENGINE, json_serializer=custom_serializer)
 
 
 def create_db_and_tables():
