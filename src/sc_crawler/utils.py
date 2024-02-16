91682ad0124530666e751dc1fff7a06dabe6ccd3
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-02-16 23:25:47 +0100

    document hasher

--- src/sc_crawler/utils.py
+++ src/sc_crawler/utils.py
@@ -1,8 +1,9 @@
 from enum import Enum
 from hashlib import sha1
 from json import dumps
-from sqlmodel import Session, create_engine
 from typing import List, Union
+
+from sqlmodel import Session, create_engine
 
 from .schemas import tables
 
@@ -18,7 +19,16 @@ def hash_database(
     level: HashLevels = HashLevels.DATABASE,
     ignored: List[str] = ["inserted_at"],
 ) -> Union[str, dict]:
-    """Hash the content of a database."""
+    """Hash the content of a database.
+
+    Args:
+        connection_string: SQLAlchemy connection string to connect to the database.
+        level: The level at which to apply hashing. Possible values are 'DATABASE' (default), 'TABLE', or 'ROW'.
+        ignored: List of column names to be ignored during hashing.
+
+    Returns:
+        A single SHA1 hash or dict of hashes, depending on the level.
+    """
     engine = create_engine(connection_string)
 
     with Session(engine) as session:
