6f17e5ea68022e7fffd6227691627bdc12b19835
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-02-21 23:43:30 +0100

    provide all required getters as dummy fns

--- src/sc_crawler/vendors/gcp.py
+++ src/sc_crawler/vendors/gcp.py
@@ -1,2 +1,34 @@
-def get_instance_types(*args, **kwargs):
-    return []
+def get_compliance_frameworks(vendor):
+    pass
+
+
+def get_datacenters(vendor):
+    pass
+
+
+def get_zones(vendor):
+    pass
+
+
+def get_servers(vendor):
+    pass
+
+
+def get_server_prices(vendor):
+    pass
+
+
+def get_server_prices_spot(vendor):
+    pass
+
+
+def get_storage_prices(vendor):
+    pass
+
+
+def get_traffic_prices(vendor):
+    pass
+
+
+def get_ipv4_prices(vendor):
+    pass
