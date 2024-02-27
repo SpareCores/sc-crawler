085f2788aaa340e218ac50d9e8e61f7c451870a5
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-02-27 15:05:38 +0100

    lint fix import order

--- src/sc_crawler/vendors/aws.py
+++ src/sc_crawler/vendors/aws.py
@@ -2,7 +2,7 @@ import json
 import re
 from datetime import datetime, timedelta
 from itertools import chain
-from typing import Tuple, List
+from typing import List, Tuple
 
 import boto3
 from cachier import cachier, set_default_params
