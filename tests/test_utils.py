11913de79b0ece58fa013e63494553c7aa42f87c
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-03-10 21:47:59 +0100

    missed list type

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,4 +5,4 @@ def test_chunk_list():
     res = [list(x) for x in chunk_list(range(10), 3)]
     assert len(res) == 4
     assert res[0] == [0, 1, 2]
-    assert res[3] == 9
+    assert res[3] == [9]
