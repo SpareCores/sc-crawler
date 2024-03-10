7a8e79d7508893ab43f01f9039fff240b3c47523
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-03-10 21:55:08 +0100

    test scmodels_to_dict

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
-from sc_crawler.utils import chunk_list
+from sc_crawler.lookup import compliance_frameworks
+from sc_crawler.utils import chunk_list, scmodels_to_dict
 
 
 def test_chunk_list():
@@ -6,3 +7,18 @@ def test_chunk_list():
     assert len(res) == 4
     assert res[0] == [0, 1, 2]
     assert res[3] == [9]
+
+
+def test_scmodels_to_dict_by_single_id():
+    cflist = [v for _, v in compliance_frameworks.items()]
+    cfdict = scmodels_to_dict(cflist, keys=["id"])
+    assert isinstance(cfdict, dict)
+    assert len(cfdict) == len(cflist)
+    assert list(cfdict.items())[0][1] == cflist[0]
+
+
+def test_scmodels_to_dict_by_multiple_ids():
+    cflist = [v for _, v in compliance_frameworks.items()]
+    cfdict = scmodels_to_dict(cflist, keys=["id", "abbreviation"])
+    assert len(cfdict) == len(cflist) * 2
+    assert list(cfdict.items())[0][1] == cflist[0]
