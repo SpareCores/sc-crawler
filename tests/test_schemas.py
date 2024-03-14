24a61b9edeabb3fcd6debadd3751470cecc471d1
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-03-14 22:24:23 +0100

    fix id -> vendor_id in tests

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -6,21 +6,21 @@ def test_bad_vendor_definition():
     # TODO ValidationError once SQLModel supports pydantic typehint validation
     with pytest.raises(ValueError):
         Vendor()
-        Vendor(id="foobar")
-        Vendor(id="foobar", name="foobar")
-        Vendor(id="foobar", name="foobar", homepage="https://foobar")
+        Vendor(vendor_id="foobar")
+        Vendor(vendor_id="foobar", name="foobar")
+        Vendor(vendor_id="foobar", name="foobar", homepage="https://foobar")
         Vendor(
-            id="foobar",
+            vendor_id="foobar",
             name="foobar",
             homepage="https://foobar",
             country=Country(id="US"),
         )
     with pytest.raises(NotImplementedError):
         Vendor(
-            id="foobar",
+            vendor_id="foobar",
             name="foobar",
             homepage="https://foobar",
-            country=Country(id="US"),
+            country=Country(country_id="US"),
             founding_year=2042,
         )
 
