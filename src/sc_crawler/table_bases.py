a722116072abc24cf6cb8ce9fe8b6219913bef5d
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-06-02 23:30:02 +0200

    enforce auto-naming constrains

--- src/sc_crawler/table_bases.py
+++ src/sc_crawler/table_bases.py
@@ -54,6 +54,16 @@ class ScMetaModel(SQLModel.__class__):
             return
         satable = subclass.metadata.tables[subclass.__tablename__]
 
+        # enforce auto-naming constrains as per
+        # https://alembic.sqlalchemy.org/en/latest/naming.html
+        subclass.metadata.naming_convention = {
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_%(constraint_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+
         # table comment
         if subclass.__doc__ and satable.comment is None:
             satable.comment = subclass.__doc__.splitlines()[0]
