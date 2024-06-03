290068356c5c6089ee0cd902779e074d17779892
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-06-03 21:25:38 +0200

    document custom hashable dict classes

--- src/sc_crawler/table_fields.py
+++ src/sc_crawler/table_fields.py
@@ -10,17 +10,25 @@ from sqlmodel import JSON
 
 
 class HashableDict(dict):
+    """A dict that can be hashed by its JSON representation.
+
+    Useful for typehinting dict-type table columns that are primary
+    keys (which need to be hashable for SQLAlchemy ORM). See
+    [][HashableDict] class for the related `sa_type`.
+    """
+
     def __hash__(self):
         return hash(dumps(self, sort_keys=True))
 
 
 class HashableJSON(TypeDecorator):
+    """Alternative JSON SQLAlchemy column representation, which can be hashed."""
+
     impl = JSON
 
     def process_result_value(self, value: str, dialect: Any) -> Any:
         if value is None:
             return None
-        # return HashableDict(loads(value))
         return HashableDict(value)
 
 
