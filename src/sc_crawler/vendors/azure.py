733a4f9580637c5107c7d924099a171179b35c5c
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-08-08 23:57:04 +0200

    add manual data for regions

--- src/sc_crawler/vendors/azure.py
+++ src/sc_crawler/vendors/azure.py
@@ -44,10 +44,394 @@ def inventory_compliance_frameworks(vendor):
 
 
 def inventory_regions(vendor):
-    """List all regions via API call."""
+    """List all regions via API call.
+
+    Location (country and state) and founding year
+    were collected manually from
+    <https://datacenters.microsoft.com/globe/explore/>
+    and its underlying JSON at
+    <https://datacenters.microsoft.com/globe/data/geo/regions.json>.
+
+    City and the energy source information was collected from
+    the sustainability fact sheets referenced in the above page and JSON.
+
+    Coordinates were provided by the Microsoft API, which doesn't seem
+    to be very reliable.
+    """
+
+    manual_datas = {
+        # Canada
+        "canadaeast": {
+            "country_id": "CA",
+            "state": "Quebec",
+            "city": "Quebec City",
+            "founding_year": 2016,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "canadacentral": {
+            "country_id": "CA",
+            "city": "Toronto",
+            "founding_year": 2016,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        # United States
+        "centralus": {
+            "country_id": "US",
+            "state": "Iowa",
+            "founding_year": 2014,
+            "green_energy": True,
+        },
+        "centraluseuap": {
+            "country_id": "US",
+            "state": "Iowa",
+            "green_energy": True,
+        },
+        "eastus": {
+            "country_id": "US",
+            "city": "Boydton",
+            "state": "Virginia",
+            # official site says 2014 with a dead link, but it was 2012 as per
+            # https://web.archive.org/web/20120530115120/http:/blogs.msdn.com/b/windowsazure/archive/2012/04/05/announcing-new-datacenter-options-for-windows-azure.aspx
+            "founding_year": 2012,
+            "green_energy": False,
+        },
+        "eastusstg": {
+            "country_id": "US",
+            "state": "Virginia",
+            "green_energy": False,
+        },
+        "eastus2": {
+            "country_id": "US",
+            "city": "Boydton",
+            "state": "Virginia",
+            # official site says 2012 with a dead link, but it was 2014 as per
+            # https://azure.microsoft.com/en-us/updates/general-availability-microsoft-azure-us-central-and-us-east-2-regions/
+            "founding_year": 2014,
+            "green_energy": False,
+        },
+        "eastus2euap": {
+            "country_id": "US",
+            "state": "Virginia",
+            "green_energy": False,
+        },
+        "northcentralus": {
+            "country_id": "US",
+            "city": "Chicago",
+            "state": "Illinois",
+            "founding_year": 2009,
+            "green_energy": False,
+        },
+        "southcentralus": {
+            "country_id": "US",
+            "state": "Texas",
+            "city": "San Antonio",
+            "founding_year": 2008,
+            "green_energy": True,
+        },
+        "westcentralus": {
+            "country_id": "US",
+            "state": "Wyoming",
+            "city": "Cheyenne",
+            "founding_year": 2016,
+            "green_energy": False,
+        },
+        "westus": {
+            "country_id": "US",
+            "state": "California",
+            "founding_year": 2012,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "westus2": {
+            "country_id": "US",
+            "state": "Washington",
+            "founding_year": 2007,
+            "green_energy": False,
+        },
+        "westus3": {
+            "country_id": "US",
+            "state": "Arizona",
+            "city": "Phoenix",
+            "founding_year": 2021,
+            "green_energy": False,
+        },
+        # Mexico
+        "mexicocentral": {
+            "country_id": "ZA",
+            "state": "Querétaro",
+            "founding_year": 2024,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        # South America
+        "brazilsouth": {
+            "country_id": "BR",
+            "state": "Campinas",
+            "founding_year": 2014,
+            "green_energy": False,
+        },
+        "brazilsoutheast": {
+            "country_id": "US",
+            "city": "Rio de Janeiro",
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        # not production region?
+        # https://github.com/Azure/azure-dev/issues/2165#issuecomment-1542948509
+        "brazilus": {
+            "country_id": "BR",
+        },
+        # Asia Pacific
+        "australiacentral": {
+            "country_id": "AU",
+            "city": "Canberra",
+            "founding_year": 2018,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "australiacentral2": {
+            "country_id": "AU",
+            "city": "Canberra",
+            "founding_year": 2018,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "australiaeast": {
+            "country_id": "AU",
+            "city": "Sydney",
+            "state": "New South Wales",
+            "founding_year": 2014,
+            "green_energy": False,
+        },
+        "australiasoutheast": {
+            "country_id": "AU",
+            "city": "Melbourne",
+            "state": "Victoria",
+            "founding_year": 2014,
+            "green_energy": False,
+        },
+        "eastasia": {
+            "country_id": "HK",
+            "founding_year": 2010,
+            "green_energy": False,
+        },
+        "southeastasia": {
+            "country_id": "SG",
+            "city": "Singapore",
+            "founding_year": 2010,
+            "green_energy": False,
+        },
+        "japaneast": {
+            "country_id": "JP",
+            "city": "Tokyo",
+            "founding_year": 2014,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "japanwest": {
+            "country_id": "JP",
+            "city": "Osaka",
+            "founding_year": 2014,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "jioindiacentral": {
+            "country_id": "IN",
+            "city": "Nagpur",
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "jioindiawest": {
+            "country_id": "IN",
+            "city": "Jamnagar",
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "centralindia": {
+            "country_id": "IN",
+            "state": "Pune",
+            "founding_year": 2015,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "southindia": {
+            "country_id": "IN",
+            "state": "Chennai",
+            "founding_year": 2015,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "westindia": {
+            "country_id": "IN",
+            "state": "Mumbai",
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "koreacentral": {
+            "country_id": "KR",
+            "city": "Seoul",
+            "founding_year": 2017,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "koreasouth": {
+            "country_id": "KR",
+            "city": "Busan",
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        # Europe
+        "francecentral": {
+            "country_id": "FR",
+            "city": "Paris",
+            "founding_year": 2018,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "francesouth": {
+            "country_id": "FR",
+            "city": "Marseille",
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "germanynorth": {
+            "country_id": "DE",
+            "city": "Berlin",
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "germanywestcentral": {
+            "country_id": "DE",
+            "city": "Frankfurt",
+            "founding_year": 2019,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "italynorth": {
+            "country_id": "IT",
+            "city": "Milan",
+            "founding_year": 2023,
+            "green_energy": False,
+        },
+        "northeurope": {
+            "country_id": "IE",
+            "city": "Dublin",
+            "founding_year": 2009,
+            "green_energy": False,
+        },
+        "norwayeast": {
+            "country_id": "NO",
+            "city": "Oslo",
+            "founding_year": 2019,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "norwaywest": {
+            "country_id": "NO",
+        },
+        "polandcentral": {
+            "country_id": "PL",
+            "city": "Warsaw",
+            "founding_year": 2023,
+            "green_energy": False,
+        },
+        "spaincentral": {
+            "country_id": "ES",
+            "city": "Madrid",
+            "founding_year": 2024,
+            "green_energy": False,
+        },
+        "swedencentral": {
+            "country_id": "SE",
+            "city": "Gävle and Sandviken",
+            "founding_year": 2021,
+            "green_energy": False,
+        },
+        "switzerlandnorth": {
+            "country_id": "CH",
+            "city": "Zürich",
+            "founding_year": 2019,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "switzerlandwest": {
+            "country_id": "CH",
+            "city": "Geneva",
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "uksouth": {
+            "country_id": "UK",
+            "city": "London",
+            "founding_year": 2016,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "ukwest": {
+            "country_id": "UK",
+            "city": "Cardiff",
+            "founding_year": 2017,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "westeurope": {
+            "country_id": "NL",
+            "founding_year": 2010,
+            "green_energy": False,
+        },
+        # Middle East
+        "israelcentral": {
+            "country_id": "IL",
+            "founding_year": 2023,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "qatarcentral": {
+            "country_id": "QA",
+            "city": "Doha",
+            "founding_year": 2022,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "uaecentral": {
+            "country_id": "AE",
+            "city": "Abu Dhabi",
+        },
+        "uaenorth": {
+            "country_id": "AE",
+            "city": "Dubai",
+            "founding_year": 2019,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        # Africa
+        "southafricanorth": {
+            "country_id": "ZA",
+            "city": "Johannesburg",
+            "founding_year": 2019,
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        "southafricawest": {
+            "country_id": "ZA",
+            "city": "Cape Town",
+            # unknown as no sustainability fact sheet found
+            "green_energy": False,
+        },
+        # China TODO enable
+    }
+
     items = []
     for region in _regions():
         if region["metadata"]["region_type"] == "Physical":
+            manual_data = manual_datas.get(region["name"], {})
+            if not manual_data:
+                import pdb
+
+                pdb.set_trace()
+
             items.append(
                 {
                     "vendor_id": vendor.vendor_id,
@@ -55,20 +439,17 @@ def inventory_regions(vendor):
                     "name": region["display_name"],
                     "api_reference": region["name"],
                     "display_name": (
-                        region["display_name"]
-                        + " ("
-                        + region["metadata"]["geography_group"]  # TODO
-                        + ")"
+                        region["display_name"] + " (" + manual_data["country_id"] + ")"
                     ),
-                    "country_id": region["metadata"]["geography_group"],  # TODO
-                    "state": None,
-                    "city": region["metadata"]["physical_location"],  # TODO
+                    "country_id": manual_data["country_id"],
+                    "state": manual_data.get("state"),
+                    "city": manual_data.get("city"),
                     "address_line": None,
                     "zip_code": None,
                     "lat": region["metadata"]["latitude"],
                     "lon": region["metadata"]["longitude"],
-                    "founding_year": None,  # TODO
-                    "green_energy": False,  # TODO
+                    "founding_year": manual_data.get("founding_year"),
+                    "green_energy": manual_data.get("green_energy"),
                 }
             )
     return items
@@ -208,7 +589,7 @@ def inventory_traffic_prices(vendor):
 
 
 def inventory_ipv4_prices(vendor):
-    # items = []
+    items = []
     # for price in []:
     #     items.append(
     #         {
