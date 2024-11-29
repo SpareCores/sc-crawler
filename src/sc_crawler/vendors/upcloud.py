9649fd4c87494cbbed492beed6b5528e66dbb1c9
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-11-29 22:55:36 +0100

    welcome upcloud regions

---
+++ src/sc_crawler/vendors/upcloud.py
@@ -0,0 +1,349 @@
+import os
+from functools import cache
+
+from upcloud_api import CloudManager
+
+from ..lookup import map_compliance_frameworks_to_vendor
+
+
+@cache
+def _client() -> CloudManager:
+    """Authorized Hetzner Cloud client using the HCLOUD_TOKEN env var."""
+    try:
+        username = os.environ["UPCLOUD_USERNAME"]
+    except KeyError:
+        raise KeyError("Missing environment variable: UPCLOUD_USERNAME")
+    try:
+        password = os.environ["UPCLOUD_PASSWORD"]
+    except KeyError:
+        raise KeyError("Missing environment variable: UPCLOUD_PASSWORD")
+    manager = CloudManager(username, password)
+    manager.authenticate()
+    return manager
+
+
+# _client().get_prices()
+# servers = _client().get_server_sizes()
+# servers[1]
+
+# templates = _client().get_templates()
+# templates[1]
+
+# _client().get_server_plans()
+
+# prices = _client().get_prices()
+# prices["prices"]["zone"][1]["server_plan_HIMEM-24xCPU-512GB"]  # EUR cent!
+
+
+def inventory_compliance_frameworks(vendor):
+    """Manual list of known compliance frameworks at UpCloud.
+
+    Data collected from their Security and Standards docs at
+    <https://upcloud.com/security-privacy>."""
+    return map_compliance_frameworks_to_vendor(
+        vendor.vendor_id,
+        ["iso27001"],
+    )
+
+
+def inventory_regions(vendor):
+    """List all regions via API call.
+
+    Data manually enriched from https://upcloud.com/data-centres."""
+    manual_data = {
+        "au-syd1": {
+            "country_id": "AU",
+            "state": "New South Wales",
+            "city": "Sydney",
+            "founding_year": 2021,
+            "green_energy": False,
+            "lon": 151.189377,
+            "lat": -33.918251,
+        },
+        "de-fra1": {
+            "country_id": "DE",
+            "state": "Hesse",
+            "city": "Frankfurt",
+            "founding_year": 2015,
+            "green_energy": True,
+            "lon": 8.735120,
+            "lat": 50.119190,
+        },
+        "fi-hel1": {
+            "country_id": "FI",
+            "state": "Uusimaa",
+            "city": "Helsinki",
+            "founding_year": 2011,
+            "green_energy": True,
+            "lon": 24.778570,
+            "lat": 60.20323,
+        },
+        "fi-hel2": {
+            "country_id": "FI",
+            "state": "Uusimaa",
+            "city": "Helsinki",
+            "founding_year": 2018,
+            "green_energy": True,
+            "lon": 24.876350,
+            "lat": 60.216209,
+        },
+        "es-mad1": {
+            "country_id": "ES",
+            "state": "Madrid",
+            "city": "Madrid",
+            "founding_year": 2020,
+            "green_energy": True,
+            "lon": -3.6239873,
+            "lat": 40.4395019,
+        },
+        "nl-ams1": {
+            "country_id": "NL",
+            "state": "Noord Holland",
+            "city": "Amsterdam",
+            "founding_year": 2017,
+            "green_energy": True,
+            "lon": 4.8400019,
+            "lat": 52.3998291,
+        },
+        "pl-waw1": {
+            "country_id": "PL",
+            "state": "Mazowieckie",
+            "city": "Warsaw",
+            "founding_year": 2020,
+            "green_energy": True,
+            "lon": 20.9192823,
+            "lat": 52.1905901,
+        },
+        "se-sto1": {
+            "country_id": "SE",
+            "state": "Stockholm",
+            "city": "Stockholm",
+            "founding_year": 2015,
+            "green_energy": True,
+            "lon": 18.102788,
+            "lat": 59.2636708,
+        },
+        "sg-sin1": {
+            "country_id": "SG",
+            "state": "Singapore",
+            "city": "Singapore",
+            "founding_year": 2017,
+            "green_energy": True,
+            "lon": 103.7022636,
+            "lat": 1.3172304,
+        },
+        "uk-lon1": {
+            "country_id": "GB",
+            "state": "London",
+            "city": "London",
+            "founding_year": 2012,
+            "green_energy": True,
+            # approximate .. probably business address
+            "lon": -0.1037341,
+            "lat": 51.5232232,
+        },
+        "us-chi1": {
+            "country_id": "US",
+            "state": "Illinois",
+            "city": "Chicago",
+            "founding_year": 2014,
+            "green_energy": False,
+            "lon": -87.6342056,
+            "lat": 41.8761287,
+        },
+        "us-nyc1": {
+            "country_id": "US",
+            "state": "New York",
+            "city": "New York",
+            "founding_year": 2020,
+            "green_energy": False,
+            "lon": -74.0645536,
+            "lat": 40.7834325,
+        },
+        "us-sjo1": {
+            "country_id": "US",
+            "state": "California",
+            "city": "San Jose",
+            "founding_year": 2018,
+            "green_energy": False,
+            "lon": -121.9754458,
+            "lat": 37.3764769,
+        },
+    }
+    items = []
+    regions = _client().get_zones()["zones"]["zone"]
+    for region in regions:
+        if region["public"] == "yes":
+            if region["id"] not in manual_data:
+                raise ValueError(f"Missing manual data for {region['id']}")
+            region_data = manual_data[region["id"]]
+            items.append(
+                {
+                    "vendor_id": vendor.vendor_id,
+                    "region_id": region["id"],
+                    "name": region["description"],
+                    "api_reference": region["id"],
+                    "display_name": (
+                        region["description"] + f" ({region_data['country_id']})"
+                    ),
+                    "aliases": [],
+                    "country_id": region_data["country_id"],
+                    "state": region_data["state"],
+                    "city": region_data["city"],
+                    "address_line": None,
+                    "zip_code": None,
+                    "lon": region_data["lon"],
+                    "lat": region_data["lat"],
+                    "founding_year": region_data["founding_year"],
+                    "green_energy": region_data["green_energy"],
+                }
+            )
+    return items
+
+
+def inventory_zones(vendor):
+    items = []
+    # for zone in []:
+    #     items.append({
+    #         "vendor_id": vendor.vendor_id,
+    #         "region_id": "",
+    #         "zone_id": "",
+    #         "name": "",
+    #     })
+    return items
+
+
+def inventory_servers(vendor):
+    items = []
+    # for server in []:
+    #     items.append(
+    #         {
+    #             "vendor_id": vendor.vendor_id,
+    #             "server_id": ,
+    #             "name": ,
+    #             "description": None,
+    #             "vcpus": ,
+    #             "hypervisor": None,
+    #             "cpu_allocation": CpuAllocation....,
+    #             "cpu_cores": None,
+    #             "cpu_speed": None,
+    #             "cpu_architecture": CpuArchitecture....,
+    #             "cpu_manufacturer": None,
+    #             "cpu_family": None,
+    #             "cpu_model": None,
+    #             "cpu_l1_cache: None,
+    #             "cpu_l2_cache: None,
+    #             "cpu_l3_cache: None,
+    #             "cpu_flags: [],
+    #             "cpus": [],
+    #             "memory_amount": ,
+    #             "memory_generation": None,
+    #             "memory_speed": None,
+    #             "memory_ecc": None,
+    #             "gpu_count": 0,
+    #             "gpu_memory_min": None,
+    #             "gpu_memory_total": None,
+    #             "gpu_manufacturer": None,
+    #             "gpu_family": None,
+    #             "gpu_model": None,
+    #             "gpus": [],
+    #             "storage_size": 0,
+    #             "storage_type": None,
+    #             "storages": [],
+    #             "network_speed": None,
+    #             "inbound_traffic": 0,
+    #             "outbound_traffic": 0,
+    #             "ipv4": 0,
+    #         }
+    #     )
+    return items
+
+
+def inventory_server_prices(vendor):
+    items = []
+    # for server in []:
+    #     items.append({
+    #         "vendor_id": ,
+    #         "region_id": ,
+    #         "zone_id": ,
+    #         "server_id": ,
+    #         "operating_system": ,
+    #         "allocation": Allocation....,
+    #         "unit": PriceUnit.HOUR,
+    #         "price": ,
+    #         "price_upfront": 0,
+    #         "price_tiered": [],
+    #         "currency": "USD",
+    #     })
+    return items
+
+
+def inventory_server_prices_spot(vendor):
+    return []
+
+
+def inventory_storages(vendor):
+    items = []
+    # for storage in []:
+    #     items.append(
+    #         {
+    #             "storage_id": ,
+    #             "vendor_id": vendor.vendor_id,
+    #             "name": ,
+    #             "description": None,
+    #             "storage_type": StorageType....,
+    #             "max_iops": None,
+    #             "max_throughput": None,
+    #             "min_size": None,
+    #             "max_size": None,
+    #         }
+    #     )
+    return items
+
+
+def inventory_storage_prices(vendor):
+    items = []
+    # for price in []:
+    #     items.append(
+    #         {
+    #             "vendor_id": vendor.vendor_id,
+    #             "region_id": ,
+    #             "storage_id": ,
+    #             "unit": PriceUnit.GB_MONTH,
+    #             "price": ,
+    #             "currency": "USD",
+    #         }
+    #     )
+    return items
+
+
+def inventory_traffic_prices(vendor):
+    items = []
+    # for price in []:
+    #     items.append(
+    #         {
+    #             "vendor_id": vendor.vendor_id,
+    #             "region_id": ,
+    #             "price": ,
+    #             "price_tiered": [],
+    #             "currency": "USD",
+    #             "unit": PriceUnit.GB_MONTH,
+    #             "direction": TrafficDirection....,
+    #         }
+    #     )
+    return items
+
+
+def inventory_ipv4_prices(vendor):
+    items = []
+    # for price in []:
+    #     items.append(
+    #         {
+    #             "vendor_id": vendor.vendor_id,
+    #             "region_id": ,
+    #             "price": ,
+    #             "currency": "USD",
+    #             "unit": PriceUnit.HOUR,
+    #         }
+    #     )
+    return items
