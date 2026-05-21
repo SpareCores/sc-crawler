8ed0182100885332eebfb0134d295ce84b2cad0c
Tamás Hidvégi <tamas.hidvegi@sparecores.com>
2026-05-21 11:48:34 +0200

    Add missing comment field to VendorFields for billing notes

--- src/sc_crawler/table_bases.py
+++ src/sc_crawler/table_bases.py
@@ -460,6 +460,11 @@ class VendorFields(HasName, HasVendorIdPK):
         description="The minimum amount of time for which the Vendor bills for.",
     )
 
+    comment: Optional[str] = Field(
+        default=None,
+        description="Comment on the Vendor's billing.",
+    )
+
 
 class VendorBase(MetaColumns, VendorFields):
     pass
