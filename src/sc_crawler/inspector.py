7d56c0882e8e11b77e5354cce5cbb4f7927cc398
Tamás Hidvégi <tamas.hidvegi@sparecores.com>
2026-06-11 18:30:04 +0200

    Add 'vultr' to _boot_from_attached_network_drive in inspector.py

--- src/sc_crawler/inspector.py
+++ src/sc_crawler/inspector.py
@@ -1045,6 +1045,7 @@ def _find_storage_disks_from_lsblk(
         "hcloud": False,
         "ovh": False,
         "upcloud": True,
+        "vultr": False,
     }
 
     for d in lsblk_data.get("blockdevices", []):
