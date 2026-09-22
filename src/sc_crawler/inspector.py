df8d4270730ed4cd32ba6df9b81b6ec288a99727
Tamás Hidvégi <tamas.hidvegi@sparecores.com>
2026-09-22 15:55:06 +0200

    Reapply "Enhance error handling in pull function to track failed vendors and raise an exception if all vendors fail and update datetime handling to ensure UTC is applied consistently across vendor modules (sqlmodel 0.0.45 changed default UTC behaviour)"
    This reverts commit 87f26023a44a4e52137f4b6112870941fe39621a.

--- src/sc_crawler/inspector.py
+++ src/sc_crawler/inspector.py
@@ -3,7 +3,7 @@ import json
 import xml.etree.ElementTree as xmltree
 from atexit import register
 from contextlib import suppress
-from datetime import datetime
+from datetime import UTC, datetime
 from functools import cache
 from itertools import groupby
 from operator import itemgetter
@@ -299,6 +299,9 @@ def _observed_at(resource: Union["Server", "Database"], framework: str) -> dict:
     else:
         ts = _database_framework_meta(resource, framework)["end"]
     assert ts is not None
+    ts = datetime.fromisoformat(ts)
+    if ts.tzinfo is None:
+        ts = ts.replace(tzinfo=UTC)
     return {"observed_at": ts}
 
 
